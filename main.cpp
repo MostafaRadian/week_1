8ad86553343e220efd8c7ed5f8ba8e30938e49b8
Mostafa A. Radian <mostafa.batesta@gmail.com>
2026-09-15 12:14:59 +0300

    adding 2d matrices problems

--- main.cpp
+++ main.cpp
@@ -1,29 +1,25 @@
 #include <iostream>
 using namespace std;
 
-class Car
-{
+class Car {
 public:
     int price;
     string color;
 };
 
 // ptr stores the address of a Car object.
-void addData(Car *ptr)
-{
+void addData(Car *ptr) {
     // -> accesses members through a pointer.
     ptr->color = "Blue";
     ptr->price = 20;
 }
 
 // num is a reference (another name) for the original variable.
-void addTen(int &num)
-{
+void addTen(int &num) {
     num += 10;
 }
 
-int main()
-{
+int main() {
     /*
         ============================
         POINTER / REFERENCE EXAMPLES
@@ -45,6 +41,7 @@ int main()
     // addData(&bmw);
     // cout << bmw.color << endl << bmw.price;
 
+
     /*
         ============================
         NORMAL 2D ARRAY
@@ -57,16 +54,17 @@ int main()
     int normalMatrix[3][4] = {
         {1, 2, 3, 4},
         {5, 6, 7, 8},
-        {9, 10, 11, 12}};
+        {9, 10, 11, 12}
+    };
 
     // Print the matrix.
-    for (int i = 0; i < 3; i++)
-    {
+    for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 4; j++)
             cout << normalMatrix[i][j] << " ";
 
         cout << endl;
     }
+
 
     /*
         ============================
@@ -95,20 +93,121 @@ int main()
         dynamicMatrix[i] = new int[col];
 
     // Fill the matrix.
-    for (int i = 0; i < row; i++)
-    {
+    for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++)
             cin >> dynamicMatrix[i][j];
     }
 
     // Print the matrix.
-    for (int i = 0; i < row; i++)
-    {
+    for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++)
             cout << dynamicMatrix[i][j] << " ";
 
         cout << endl;
     }
+
+
+    /*
+        ============================
+        MATRIX PROBLEMS
+        ============================
+
+        We will practice three common
+        operations on a 2D array:
+
+        1. Find the sum of all elements.
+        2. Find the row with the largest sum.
+        3. Add two matrices together.
+    */
+
+    // Create another dynamic matrix.
+    int **matrix = new int *[row];
+
+    for (int i = 0; i < row; i++)
+        matrix[i] = new int[col];
+
+    /*
+        ----------------------------
+        1. SUM OF THE MATRIX
+        ----------------------------
+
+        Visit every element and add it
+        to the total sum.
+    */
+
+    int matrixSum = 0;
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            cin >> matrix[i][j];
+            matrixSum += matrix[i][j];
+        }
+    }
+
+    cout << "Matrix Sum: " << matrixSum << endl;
+
+
+    /*
+        ----------------------------
+        2. ROW WITH THE LARGEST SUM
+        ----------------------------
+
+        Calculate the sum of each row,
+        then keep track of the largest one.
+    */
+
+    int maxRowSum = 0;
+    int maxRow = 0;
+
+    for (int i = 0; i < row; i++) {
+        int rowSum = 0;
+
+        for (int j = 0; j < col; j++)
+            rowSum += matrix[i][j];
+
+        if (rowSum > maxRowSum) {
+            maxRowSum = rowSum;
+            maxRow = i;
+        }
+    }
+
+    cout << "Maximum Row: " << maxRow << endl;
+    cout << "Maximum Row Sum: " << maxRowSum << endl;
+
+
+    /*
+        ----------------------------
+        3. SUM OF TWO MATRICES
+        ----------------------------
+
+        Both matrices must have the same
+        number of rows and columns.
+
+        Add elements at the same position:
+        result[i][j] = A[i][j] + B[i][j]
+    */
+
+    int **sumMatrix = new int *[row];
+
+    for (int i = 0; i < row; i++)
+        sumMatrix[i] = new int[col];
+
+    // Add corresponding elements.
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++)
+            sumMatrix[i][j] = matrix[i][j] + dynamicMatrix[i][j];
+    }
+
+    // Print the resulting matrix.
+    cout << "Sum Matrix:" << endl;
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++)
+            cout << sumMatrix[i][j] << " ";
+
+        cout << endl;
+    }
+
 
     /*
         ============================
@@ -119,12 +218,17 @@ int main()
         have a matching delete/delete[].
     */
 
-    // Delete each row first.
-    for (int i = 0; i < row; i++)
+    // Delete the rows of each matrix.
+    for (int i = 0; i < row; i++) {
         delete[] dynamicMatrix[i];
+        delete[] matrix[i];
+        delete[] sumMatrix[i];
+    }
 
-    // Then delete the array of pointers.
+    // Then delete the arrays of pointers.
     delete[] dynamicMatrix;
+    delete[] matrix;
+    delete[] sumMatrix;
 
     /*
         Remember:
